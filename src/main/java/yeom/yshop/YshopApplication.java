40ec3ca97f65db14825980631b580d8bf6445e99
Yeom <ydh7219@naver.com>
2023-07-27 16:29:21 +0900

    first

---
+++ src/main/java/yeom/yshop/YshopApplication.java
@@ -0,0 +1,13 @@
+package yeom.yshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yeom/yshop/YshopApplicationTests.java
@@ -0,0 +1,13 @@
+package yeom.yshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
